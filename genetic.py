46cd2c4f4aa7b105157a525e862c7a09f3a49ef8
vkomodey <vkomodey@gmail.com>
2022-10-24 18:37:13 +0200

    First working version

--- genetic.py
+++ genetic.py
@@ -1,6 +1,66 @@
-def cross(route1: np.array, route2: np.array) -> np.array:
-    """
-        Crossing 2 routes between each other
-    """
+from typing import Tuple
+import numpy as np
+import random
+from graph import Graph
 
-    pass
+def gen_population(graph: Graph, populations: int) -> np.array:
+    perms = np.zeros([populations, graph.vertexes_amount + 1], int)
+    counter = 0
+    # we assume that we start from the first vertex
+    vertexes = graph.vertexes[1:]
+
+    while counter < populations:
+        permutation = np.random.permutation(vertexes)
+        gene = np.append(np.array([0]), permutation)
+        gene = np.append(gene, np.array([0]))
+        perms[counter] = gene
+        counter += 1
+
+    return perms
+
+
+def population_fitness(g: Graph, p: np.array) -> int:
+    s = 0
+
+    for gene in p:
+        s += g.route_fitness(gene)
+
+    return s
+
+
+def crossover(g: Graph, mother_gene: np.array, father_gene: np.array) -> np.array:
+    mother_route = mother_gene[1: -1]
+    father_route = father_gene[1: -1]
+
+    middle_idx = len(mother_route) // 2
+
+    mother_offspring = mother_route[middle_idx:]
+
+    father_offspring = np.array([vertex for vertex in father_route if vertex not in mother_offspring])
+
+    offspring_route = np.append(father_offspring, mother_offspring)
+
+    return g.complete_route(offspring_route)
+
+
+def mutate(g: Graph, gene: np.array) -> np.array:
+    route = gene[1: -1]
+
+    swap_first_idx = random.randint(1, len(gene) - 3)
+    swap_second_idx = random.randint(1, len(gene) - 3)
+
+    while swap_first_idx == swap_second_idx:
+        swap_second_idx = random.randint(1, len(gene) - 3)
+
+    route[swap_first_idx], route[swap_second_idx] = route[swap_second_idx], route[swap_first_idx]
+
+    return g.complete_route(route)
+
+def select_parents(population: np.array) -> Tuple[np.array, np.array]:
+    mother_idx = np.random.randint(0, len(population) - 1)
+
+    while (father_idx := np.random.randint(0, len(population) - 1)) == mother_idx:
+        continue
+
+    return population[mother_idx], population[father_idx]
+
--- main.py
+++ main.py
@@ -1,65 +1,46 @@
 import numpy as np
 import random
+
+np.random.seed(42)
+random.seed(42)
 from graph import Graph
+from genetic import gen_population, crossover, mutate, select_parents, population_fitness
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(22, 6))
+
 
 vertexes_amount = 10
 population_amount = 10
+mutate_threshold = 0.3
+iterations = 200
 
-g = Graph(vertexes_amount)
+graph = Graph(vertexes_amount)
 
+population = gen_population(graph, population_amount)
+total_fitness_values = [population_fitness(graph, population)]
+population_fitness_values = [graph.route_fitness(route) for route in population]
+min_fitness_values = [np.min(population_fitness_values)]
 
-def gen_population(graph: Graph, populations: int) -> np.array:
-    perms = np.zeros([populations, graph.vertexes_amount + 1], int)
-    counter = 0
-    # we assume that we start from the first vertex
-    vertexes = graph.vertexes[1:]
+for i in range(iterations):
+    children = []
+    while len(children) <= population_amount:
+        mother, father = select_parents(population)
+        child = crossover(graph, mother, father)
+        if np.random.rand() < mutate_threshold:
+            child = mutate(graph, child)
+        children.append(child)
 
-    while counter < populations:
-        permutation = np.random.permutation(vertexes)
-        gene = np.append(np.array([0]), permutation)
-        gene = np.append(gene, np.array([0]))
-        perms[counter] = gene
-        counter += 1
+    total_fitness = population_fitness(graph, children)
+    total_fitness_values.append(total_fitness)
+    population_fitness_values = [graph.route_fitness(route) for route in children]
+    min_fitness_values.append(np.min(population_fitness_values))
 
-    return perms
-
-
-def population_fitness(g: Graph, p: np.array) -> int:
-    s = 0
-
-    for gene in p:
-        s += g.route_fitness(gene)
-
-    return s
+    population = np.array(children)
 
 
-def crossover(mother_gene: np.array, father_gene: np.array) -> np.array:
-    mother_route = mother_gene[1: -1]
-    father_route = father_gene[1: -1]
-
-    middle_idx = len(mother_route) // 2
-
-    mother_offspring = mother_route[middle_idx:]
-
-    father_offspring = np.array([vertex for vertex in father_route if vertex not in mother_offspring])
-
-    offspring_route = np.append(father_offspring, mother_offspring)
-
-    return g.complete_route(offspring_route)
-
-
-def mutate(g: Graph, gene: np.array) -> np.array:
-    route = gene[1: -1]
-
-    swap_first_idx = random.randint(1, len(gene) - 3)
-    swap_second_idx = random.randint(1, len(gene) - 3)
-
-    while swap_first_idx == swap_second_idx:
-        swap_second_idx = random.randint(1, len(gene) - 3)
-
-    route[swap_first_idx], route[swap_second_idx] = route[swap_second_idx], route[swap_first_idx]
-
-    return g.complete_route(route)
-
-
-print(mutate(g, np.array([0,1,2,3,4,0])))
+plt.plot(min_fitness_values)
+plt.title("Min Fitness Values")
+plt.xticks(np.arange(0, iterations, 10))
+plt.grid()
+plt.show()
